3a03299f1d2c80dfaeb49741f7a5ce91bd077a41
Duncan Parkes <duncan@mysociety.org>
2015-05-12 17:04:49 +0100

    First go at scraping data for the House of Representatives

--- scraper.py
+++ scraper.py
@@ -1,19 +1,64 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import re
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
+import requests
+import lxml.html
+
+import scraperwiki
+
+resp = requests.get('http://house.gov.by/index.php/,7508,,,,1,,,0.html')
+root = lxml.html.fromstring(resp.text)
+
+trs = root.xpath("//tr[td[a[@class='d_list']]]")
+
+data = []
+
+for tr in trs:
+    member = {'chamber': 'House of Representatives'}
+    member['name'] = tr.xpath('.//a')[0].text_content().strip()
+    member['details_url'] = tr.xpath('.//a')[0].get('href')
+    member['constituency'] = tr.xpath('td')[-1].text_content().strip()
+    member['constituency_id'] = int(re.search('\d+', member['constituency']).group())
+
+    member_resp = requests.get(member['details_url'])
+    member_root = lxml.html.fromstring(member_resp.text)
+
+    email_texts = member_root.xpath("//p[contains(., 'E-mail')]")
+
+    if len(email_texts) > 1:
+        print "Lots of emails!"
+        import pdb;pdb.set_trace()
+    elif len(email_texts) == 1:
+        email_text = email_texts[0].text_content().strip()
+
+        if email_text == 'E-mail:':
+            print "No email"
+        elif email_text.startswith('E-mail:'):
+            member['email'] = email_text[7:].strip()
+        else:
+            print "Bad email"
+    else:
+        print "No email"
+
+        # A couple of people have the following instead of 'E-mail:'
+        # E-ma\xb3l:
+
+    data.append(member)
+    scraperwiki.sqlite.save(unique_keys=['name'], data=data)
+
+
+print "Missing constituency ids: " + repr(set(range(1, 111)) - set([x['constituency_id'] for x in data]))
+
+
+
+
+
+
 # # Find something on the page using css selectors
 # root = lxml.html.fromstring(html)
 # root.cssselect("div[align='left']")
 #
 # # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
+
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
 
